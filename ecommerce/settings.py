077b7028407459e9e898a1d9f0a96daa2fa8768f
jayuchawla78 <jayprakashchawla78@gmail.com>
2023-05-01 03:03:25 +0530

    base html - store - navbar, basic store

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Path where static files are stored
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
