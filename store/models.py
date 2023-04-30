49c87b761039bd150732636e570928a0dbdcae34
jayuchawla78 <jayprakashchawla78@gmail.com>
2023-04-30 15:58:14 +0530

    admin configuration: prepopulate slug field

--- store/models.py
+++ store/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255, db_index=True, default="un-categorized")
-    slug = models.SlugField(max_length=250, unique=True, default="un-categorized")
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
         
     class Meta:
         # override django's defualt behaviour to avoid categorys 
@@ -14,12 +14,12 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    title = models.CharField(max_length=250, default="un-titled-product")
+    title = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='un-branded')
     description = models.TextField(blank=True)
-    slug = models.SlugField(max_length=255, default="no-product")
-    price = models.DecimalField(max_digits=4, decimal_places=2, null=True)
-    image = models.ImageField(upload_to='images/', null=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
 
     class Meta:
         verbose_name_plural = 'products'
