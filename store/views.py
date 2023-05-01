8ad881ff7e684d2c5d5cc377c69ae2e75979cf86
jayuchawla78 <jayprakashchawla78@gmail.com>
2023-05-01 19:52:36 +0530

    iterate on all products to display on store home

--- store/views.py
+++ store/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from . import models
 
 # Create your views here.
+
+
 def store(request):
-    return render(request, 'store/store.html')
+    return render(request, 'store/store.html', {'all_products': models.Product.objects.all()})
+
 
 def categories(request):
     all_categories = models.Category.objects.all()
